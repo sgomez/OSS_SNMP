f8662ba03c1dd52d04fb60d2d1eabacbf5307933
Sergio Gómez <sergio@uco.es>
2016-10-07 08:26:16 +0200

    CISCO-PROCESS-MIB created

---
+++ src/OSS_SNMP/MIBS/Cisco/Process.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+    Copyright (c) 2012, Open Source Solutions Limited, Dublin, Ireland
+    All rights reserved.
+
+    Contact: Barry O'Donovan - barry (at) opensolutions (dot) ie
+             http://www.opensolutions.ie/
+
+    This file is part of the OSS_SNMP package.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright
+          notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of Open Source Solutions Limited nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+namespace OSS_SNMP\MIBS\Cisco;
+
+/**
+ * A class for performing SNMP V2 queries on Cisco devices
+ *
+ * @copyright Copyright (c) 2012, Open Source Solutions Limited, Dublin, Ireland
+ * @author Sergio Gómez Bachiller <sergio@uco.es>
+ */
+class Process extends \OSS_SNMP\MIBS\Cisco
+{
+    const OID_CISCO_CPM_CPU_TOTAL_5_MIN_REV = '.1.3.6.1.4.1.9.9.109.1.1.1.1.8';
+    
+    /**
+     * The overall CPU busy percentage in the last 5 minute period. This object
+     * deprecates the object cpmCPUTotal5min and increases the value range 
+     * to (0..100).
+     */
+    public function cpmCPUTotal5minRev()
+    {
+        return $this->getSNMP()->walk1d(self::OID_CISCO_CPM_CPU_TOTAL_5_MIN_REV);
+    }
+}
